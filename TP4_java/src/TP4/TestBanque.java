0b2055c92f511e37a8ceb30ef51373e85e8896d8
youneschakir01 <younes.chakir.08@edu.uiz.ac.ma>
2026-04-11 23:32:26 +0100

    Update TestBanque.java

--- TP4_java/src/TP4/TestBanque.java
+++ TP4_java/src/TP4/TestBanque.java
@@ -25,16 +25,3 @@ public class TestBanque {
 		}
 	}
 }
-/*
-Compte c = new CompteEpargne();
-CompteCourant cc = (CompteCourant) c;
-❌ Est-ce que ça fonctionne ?
-👉 NON, ça ne fonctionne pas correctement à l’exécution.
-❌ Erreur :
-👉 ClassCastException
-❓ Pourquoi ?
-c est réellement un objet de type CompteEpargne
-tu essaies de le convertir en CompteCourant
-ce sont deux classes différentes (frères dans l’héritage)
-Java refuse ce cast à runtime
- */
